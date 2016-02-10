3462ccb94fe1e5f8a9775ef14b41267d0bb76786
Andriy Luchko <andruluchko@ukr.net>
2016-02-10 18:33:36 +0200

    Added filters

--- js/controllers.js
+++ js/controllers.js
@@ -3,5 +3,6 @@ var app = angular.module("myApp", []);
 app.controller("MyController", ["$scope", "$http", function($scope, $http) {
   $http.get("js/data.json").success(function(data){
     $scope.artists = data;
+    $scope.artistOrder = "name";
   });
 }]);
